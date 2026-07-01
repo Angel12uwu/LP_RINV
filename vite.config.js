74035d15f4de618dacfb80a1c643aa71ce060147
AngelLagos <angelagosauceda@gmail.com>
2026-06-30 20:08:02 -0600

    update index.html, cardholder.hbs, and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import {defineConfig} from "vite";
-import path, {resolve} from "node:path";
+import { defineConfig } from "vite";
+import path, { resolve } from 'node:path';
 import * as glob from "glob";
 
-import HtmlCssPurgePlugin from "vite-plugin-purgecss";
-import HandlebarPlugin from "vite-plugin-handlebars";
+import HtmlCssPurgePlugin from 'vite-plugin-purgecss';
+import HandlebarPlugin from 'vite-plugin-handlebars';
 
-function obtenerHtmlFiles(){
+import getPageContext from './src/data/index';
+
+
+function obtenerHtmlFiles() {
     return Object.fromEntries(
         glob.sync(
             './**/*.html',
@@ -16,32 +19,41 @@ function obtenerHtmlFiles(){
                 ]
             }
         ).map((file)=>{
-            return[
-                file.slice(0, file.length - path.extname(file).length), //nombre del archivo sin extension
-                resolve(__dirname, file) //full path a el archivo
+            return [
+                file.slice(0, file.length - path.extname(file).length), // nombre del archivo sin extensión
+                resolve(__dirname, file) // full path a el archivo
             ]
         })
     );
     /*
         {
-            "index": "/full/path/to/index.html",
-            "productos": "/full/path/to/productos.html",
+            "index": "~/....../index.html",
+            "productos": "~/......../productos.html",
         }
     */
 }
 
-export default defineConfig({
-    appType: 'mpa',
-    build:{
-        rolldownOptions: {
-            input: obtenerHtmlFiles()
-        }
-    },
-
-    plugins: [
-        HandlebarPlugin({
-           partialDirectory: resolve(__dirname, "src/partials"), 
-        }),
-        HtmlCssPurgePlugin()
-    ]
-});
+export default defineConfig(
+    {
+        appType: 'mpa',
+        build: {
+            rolldownOptions: {
+                input: obtenerHtmlFiles()
+            }
+        },
+        plugins: [
+            HandlebarPlugin(
+                {
+                    partialDirectory: resolve(__dirname, 'src/partials'),
+                    context: (page) => {
+                        console.log(`Cargando contexto de: ${page}`);
+                        let context = getPageContext(page);
+                        console.log(JSON.stringify(context,null, 2));
+                        return context;
+                    }
+                }
+            ),
+            HtmlCssPurgePlugin()
+        ]
+    }
+);
