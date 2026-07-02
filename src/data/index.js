2be517865832af2eb1179bc8c371bfef5b7fcd91
AngelLagos <angelagosauceda@gmail.com>
2026-07-01 20:20:00 -0600

    update: added product page and handlebars

--- src/data/index.js
+++ src/data/index.js
@@ -1,11 +1,12 @@
 import indexContext from './pages/index_page';
+import productosContext from './pages/productos_page';
 
 export default (page)=>{
     switch(page){
         case '/index.html':
             return indexContext;
         case '/productos.html':
-            return {};
+            return productosContext;
         default:
             return {};
     }
